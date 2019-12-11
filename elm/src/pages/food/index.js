b2c41994aa8150c09009961e8358c03609a685f3
wwt98520 <573055072@qq.com>
2019-12-11 09:03:11 +0800

    add food miste

--- elm/src/pages/food/index.js
+++ elm/src/pages/food/index.js
@@ -1,11 +1,20 @@
 import React,{Component} from "react"
-
-class Header extends Component{
+import Head from "./head/head.js"
+import Btn from "./btn/btn.js"
+import "../../assets/css/food/food.css"
+import List from  "./list/list.js"
+import Footer from "./footer/footer.js"
+class Food extends Component{
     render(){
         return(
-            <div>footer</div>
+            <div className="main">
+                <Head/>
+                <Btn/>
+                <List/>
+                <Footer/>
+            </div>
         )
     }
 }
 
-export default Header;
+export default Food;
--- elm/src/pages/miste/index.js
+++ elm/src/pages/miste/index.js
@@ -1,11 +1,18 @@
-import React,{Component} from "react"
-
-class Header extends Component{
-    render(){
-        return(
-            <div>footer</div>
+import React, { Component, Fragment } from "react"
+import "../../assets/css/miste/index.css"
+import Head from "./Head/head.js"
+import List from "./list/list.js"
+class Miste extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="main">
+                    <Head/>
+                    <List/>
+                </div>
+            </Fragment>
         )
     }
 }
 
-export default Header;
+export default Miste;
